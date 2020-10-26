e84ab8d3f531d987caa96ffb9097b9108624728f
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-26 19:32:02 -0400

    Test Cases

--- BankTester.java
+++ BankTester.java
@@ -1,3 +1,9 @@
 public class BankTester{
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    BankAccount fa = new BankAccount("password", 2);
+    System.out.println(fa.getBalance());
+    System.out.println(fa.getAccountID());
+    fa.setPassword("newPass");
+    System.out.println(fa.deposit(100));
+  }
 }
