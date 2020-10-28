7988650edae810db6c01c27107b0afae34ac68b2
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-28 14:02:18 -0400

    Fixed Errors, parameters swapped

--- BankTester.java
+++ BankTester.java
@@ -1,10 +1,10 @@
 public class BankTester{
   public static void main(String[] args) {
-    BankAccount fa = new BankAccount("password", 2);
+    BankAccount fa = new BankAccount(2, "password");
     System.out.println(fa.getBalance());
     System.out.println(fa.getAccountID());
     fa.setPassword("newPass");
-    System.out.println(fa.deposit(100));
-    System.out.println(fa.deposit(200));
+    System.out.println(fa.deposit(100.0));
+    System.out.println(fa.deposit(200.0));
   }
 }
